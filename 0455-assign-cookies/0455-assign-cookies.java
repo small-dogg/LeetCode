b38a5da2de57e7e685813459469c7d990d47fa7d
박선용 <74956749+small-dogg@users.noreply.github.com>
2025-11-04 11:40:23 +0900

    Time: 13 ms (6.29%), Space: 51.7 MB (6.89%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        if(s.length == 0) return 0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int i = 0;
+        int j = 0;
+        int result = 0;
+        while (i < g.length && j < s.length) {
+            if (g[i] <= s[j]) {
+                i++;
+                j++;
+                result++;
+            } else {
+                j++;
+            }
+        }
+        return result;
+    }
+}
