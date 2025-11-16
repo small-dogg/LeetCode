b2539389717dda6e09bf3edb5fdd022f9381e9f8
박선용 <74956749+small-dogg@users.noreply.github.com>
2025-11-16 21:24:08 +0900

    Time: 0 ms (100%), Space: 42.4 MB (5.71%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -7,25 +7,24 @@ class Solution {
 
         //백트래킹 필요하고, 방향은 2개
 
-        boolean[][] visited = new boolean[m][n];
         int[][] memo = new int[m][n];
-        visited[0][0] = true;
-        return dfs(0, 0, visited, memo);
+        
+        return dfs(0, 0, memo);
     }
 
-    private int dfs(int x, int y, boolean[][] visited, int[][] memo) {
+    private int dfs(int x, int y, int[][] memo) {
         //우측 하단에 도달하면 반환
-        if (x == visited.length - 1 && y == visited[0].length - 1) return 1;
+        if (x == memo.length - 1 && y == memo[0].length - 1) return 1;
 
         //이동불가면 반환
-        if (x > visited.length - 1 || y > visited[0].length - 1) return 0;
+        if (x > memo.length - 1 || y > memo[0].length - 1) return 0;
 
         if(memo[x][y] !=0) return memo[x][y];
 
-        visited[x][y] = true;
-        int dfs = dfs(x + 1, y, visited, memo);
-        int dfs1 = dfs(x, y + 1, visited, memo);
-        visited[x][y] = false;
+
+        int dfs = dfs(x + 1, y, memo);
+        int dfs1 = dfs(x, y + 1, memo);
+
 
         memo[x][y] = dfs+dfs1;
 
