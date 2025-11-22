66911f56f949e52afce6d62a5f7b2a526964e042
박선용 <74956749+small-dogg@users.noreply.github.com>
2025-11-22 17:15:15 +0900

    Time: 13 ms (18.65%), Space: 46.5 MB (40.47%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        //nums 배열의 모든 원소는 같은수가 2개씩있음, 한개만 있는애를 찾아서 반환
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if(entry.getValue()==1) return entry.getKey();
+        }
+
+        return 0;
+    }
+}
